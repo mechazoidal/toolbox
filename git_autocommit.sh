ed0a634c2cb7e1a459621bd4d367033269ca0c76
Scott Francis <scott@kurokoproject.com>
2017-04-01 14:49:49 -0700

    Add trial 'make_archive' option

--- git_autocommit.sh
+++ git_autocommit.sh
@@ -3,13 +3,16 @@
 # Intended use-case is for autosaved plaintext notes, i.e. vimwiki and the like.
 # swf, 20120206
 
-while getopts ":vd:" opt; do
+while getopts ":vad:" opt; do
   case $opt in
     v)
       VERBOSE=1
       ;;
     d)
       DIRECTORY=$OPTARG
+      ;;
+    a)
+      MAKE_ARCHIVE=1
       ;;
     /?)
       echo "Invalid option: -$OPTARG" >&2
@@ -22,6 +25,10 @@ while getopts ":vd:" opt; do
 done
 
 # from: http://serverfault.com/a/400419
+# -m : get list of all modified files since last commit
+# -o : show untracked files
+# --exclude-standard : takes .gitignore and .git/info/exclude into account when building the list
+# --git-dir & --work-tree : must specify these when running from cronjob
 if [ -n "`git --git-dir "$DIRECTORY/.git" --work-tree "$DIRECTORY" ls-files -m -o --exclude-standard`" ] 
 then
   if [ $VERBOSE ] 
@@ -29,10 +36,15 @@ then
     echo "gonna commit to this"
   fi
 
-  # FIXME: should really get the list of added files w/ls-files
   # --all also records removal of files
   git --git-dir "$DIRECTORY/.git" --work-tree "$DIRECTORY" add --all .
   git --git-dir "$DIRECTORY/.git" --work-tree "$DIRECTORY" commit -m"AUTOCOMMIT" --quiet
+  # UNTESTED
+  if [ $MAKE_ARCHIVE ]
+  then
+    date=$(date "+%Y%m%d")
+    git archive --format=zip --prefix="$DIRECTORY/" HEAD > "$HOME/Desktop/$DIRECTORY_$date.zip"
+  fi
 else
   if [ $VERBOSE ] 
   then
