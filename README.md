71927480d47a551a2702bb63a19e4ec8d6819fe7
Scott Francis <scott@kurokoproject.com>
2020-04-03 18:09:02 -0700

    can you believe how long it's been since an update?

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 Various scripts I've written as helpers, usually command-line driven.
 
-# Ruby
-* Most of these do not manually include rubygems(use rvm, a 1.9 interpreter, or set autoloading in your .profile).
-* [Trollop](http://trollop.rubyforge.org) and [Highline](http://highline.rubyforge.org/) are used for user-input wherever possible.
--- quickshare.rb
+++ quickshare.rb
@@ -2,19 +2,19 @@
 
 require 'thin'
 require 'rack'
-#require 'socket'
-require 'trollop'
+require 'socket'
+require 'optimist'
 
 # inspired by: http://code.joejag.com/2011/start-a-web-server-from-your-pwd-using-ruby-and-thin/
 
-opts = Trollop::options do
+opts = Optimist::options do
   opt :path, "path of files that should be hosted", :default=>Dir.pwd, :type=>:string
-  #opt :hostname, "hostname", :default=>IPSocket.getaddress(Socket.gethostname)
-  opt :hostname, "hostname", :default=>"localhost"
-  opt :socket, "socket to host on", :default=>7777
+  opt :hostname, "hostname", :default=>IPSocket.getaddress(Socket.gethostname)
+  #opt :hostname, "hostname", :default=>"localhost"
+  opt :port, "port to host on", :default=>7777
 end
 
-Thin::Server.start(opts[:hostname], opts[:socket]) do
+Thin::Server.start(opts[:hostname], opts[:port]) do
   use Rack::CommonLogger
   run Rack::Directory.new(opts[:path])
 end
